6053262ea7a49db7b6c976a55a776e9f95003b7d
triplee12 <tripleeoliver@gmail.com>
2022-08-18 14:00:14 +0100

    Add user account with dashboard

---
+++ bookmark/account/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView,
+    PasswordChangeDoneView,PasswordResetView, PasswordResetCompleteView,
+    PasswordResetDoneView, PasswordResetConfirmView
+)
+from .views import dashboard, user_login
+
+urlpatterns = [
+    # path('login/', user_login, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('dashboard/', dashboard, name='dashboard'),
+]
