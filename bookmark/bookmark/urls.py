93540527dc1420a637cf971b313a4987defb413e
triplee12 <tripleeoliver@gmail.com>
2022-08-28 20:14:38 +0100

    Add images application with bookmarklet

--- bookmark/bookmark/urls.py
+++ bookmark/bookmark/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
     path('social-auth/', include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ bookmark/images/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import image_create, image_detail
+
+app_name = "images"
+
+urlpatterns = [
+    path('create/', image_create, name="create"),
+    path('detail/<int:id>/<slug:slug>/', image_detail, name='detail'),
+]
