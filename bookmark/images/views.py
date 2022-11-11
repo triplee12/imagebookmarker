bdf877ead104d29e293f7101ec274cc96ac830ac
triplee12 <tripleeoliver@gmail.com>
2022-11-11 10:21:29 +0100

    Add decorator that checks if request is ajax request

--- bookmark/images/views.py
+++ bookmark/images/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
 from .forms import ImageCreateForm
 from .models import Image
 
@@ -10,6 +11,7 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html', {'section': 'image', 'image':image})
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
