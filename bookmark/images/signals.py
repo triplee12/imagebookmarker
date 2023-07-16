e3dc707e0f9ce4f3771a394b7ba625d5dfe67bde
triplee12 <tripleeoliver@gmail.com>
2023-07-16 19:18:57 +0100

    Implement signal to calculate sum of users like

---
+++ bookmark/images/signals.py
@@ -0,0 +1,11 @@
+"""Image signal."""
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+from .models import Image
+
+
+@receiver(m2m_changed, sender=Image.user_like.through)
+def users_like_changed(sender, instance, **kwargs):
+    """Users like changed signal."""
+    instance.total_likes = instance.users_like.count()
+    instance.save()
